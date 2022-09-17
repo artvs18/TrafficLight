4d2146ffac9354a09bffe1f2394d41bff4cc68a3
Artemy Volkov <wasartemwolf@gmail.com>
2022-09-17 15:20:38 +0300

    Storyboard interface built and logic of traffic light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var buttonToChangeLight: UIButton!
+    
+    private var numberOfTaps = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        redLight.layer.cornerRadius = redLight.bounds.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.bounds.width / 2
+        greenLight.layer.cornerRadius = greenLight.bounds.width / 2
+        buttonToChangeLight.layer.cornerRadius = 15
     }
-
-
+    
+    @IBAction func buttonDidTapped() {
+        buttonToChangeLight.setTitle("NEXT", for: .normal)
+        numberOfTaps += 1
+        
+        switch numberOfTaps {
+        case 1:
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        case 2:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            greenLight.alpha = 0.3
+        default:
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            numberOfTaps = 0
+        }
+    }
 }
-
